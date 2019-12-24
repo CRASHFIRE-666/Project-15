fa13d2113885369e011d4e63d637baa0850b39de
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 14:50:06 +0300

    Update 15.2.cpp

--- 15.2.cpp
+++ 15.2.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    int n;
-    cout<<"Введите N"<<endl;
-    cin>>n;
-    vector<int> a(n);
-    cout<<"Введите массив"<<endl;
-    for (int i=0; i<n; i++)
-    {
-        cin>>a[i];
+    int n, i, j, sum, counter;
+    
+    
+    cin >> n;
+    
+    
+    int* a = new int [n];
+    
+    int* b = new int [n];
+    
+    for(i = 0; i < n; i++){
+        cin >> a[i];
     }
-    vector<float> b(n);
-    b[0]=0;
-    float sum=0;
-    for (int i=1; i<n; i++)
-    {
-        sum+=a[i];
-        b[i]=sum/i;
+    
+    for (i = 0; i < n; i++){
+        sum = 0;
+        counter = 0;
+        for(j = 0; j < i; j++){
+            sum += a[j];
+            counter++;
+        }
+        b[i] = sum / counter;
     }
-    for (int i=0; i<n; i++)
-    {
-        cout<<b[i]<<endl;
-    }
+    
+    
+    
     return 0;
 }
